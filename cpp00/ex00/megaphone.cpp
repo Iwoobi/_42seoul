3e5173e47bf203ac9c3295194f3e240f6f7f7175
Youngung Han <youhan@c2r8s2.42seoul.kr>
2022-09-16 17:02:41 +0900

    ASd

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youhan <youhan@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/16 16:12:41 by youhan            #+#    #+#             */
+/*   Updated: 2022/09/16 17:00:15 by youhan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return 0;
+	}
+	i = 1;
+	while (argv[i] != nullptr)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				std::cout << static_cast<char>(argv[i][j] + 'A' - 'a');
+			else
+				std::cout << argv[i][j];
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+}
