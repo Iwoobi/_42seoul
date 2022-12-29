12d8acb6d21ee3fbf975e2578fa74c83a5f97a7c
Youngung Han <youhan@c4r7s5.42seoul.kr>
2022-12-29 15:18:06 +0900

    asd

---
+++ cpp/cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,119 @@
+#include "Bureaucrat.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Bureaucrat::Bureaucrat() : name("")
+{
+	this->grade = 150;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade) : name(name)
+{
+	this->grade = grade;
+	try
+	{
+		if (grade > 150)
+			throw Bureaucrat::GradeTooLowException();
+		else if (grade < 1)
+			throw Bureaucrat::GradeTooHighException();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+
+Bureaucrat::Bureaucrat( const Bureaucrat & src ) : name(src.name)
+{
+	this->grade = src.grade;
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Bureaucrat &				Bureaucrat::operator=( Bureaucrat const & rhs )
+{
+	if ( this != &rhs )
+	{
+		this->grade = rhs.grade;
+	}
+	return *this;
+}
+
+std::string Bureaucrat::getName() const
+{
+	return (this->name);
+}
+
+int Bureaucrat::getGrade() const
+{
+	return (this->grade);
+}
+
+std::ostream &			operator<<( std::ostream & o, Bureaucrat const & i )
+{
+	o << i.getName() << " bureaucrat grade " << i.getGrade();
+	return o;
+}
+
+
+void	Bureaucrat::upgrade()
+{
+	try
+	{
+		if (this->grade == 1)
+			throw Bureaucrat::GradeTooHighException();
+		this->grade -= 1;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+void	Bureaucrat::downgrade()
+{
+	try
+	{
+		if (this->grade == 150)
+			throw Bureaucrat::GradeTooLowException();
+		this->grade += 1;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+}
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{	
+	return ("too high grade");
+}
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("too low grade");
+}
+
+
+/*
+** --------------------------------- METHODS ----------------------------------
+*/
+
+
+/*
+** --------------------------------- ACCESSOR ---------------------------------
+*/
+
+
+/* ************************************************************************** */
---
+++ cpp/cpp05/ex00/main.cpp
@@ -0,0 +1,6 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+	Bureaucrat a("a", 154);
+}
