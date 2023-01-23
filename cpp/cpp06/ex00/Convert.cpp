93d0b6ebbce0177cce0176a84226eb98695d2e0e
Youngung Han <youhan@c4r6s5.42seoul.kr>
2023-01-23 19:05:02 +0900

    asd

---
+++ cpp/cpp06/ex00/Convert.cpp
@@ -0,0 +1,38 @@
+#include "Convert.hpp"
+
+/*
+** ------------------------------- CONSTRUCTOR --------------------------------
+*/
+
+Convert::Convert()
+{
+}
+
+Convert::Convert( const Convert & src )
+{
+}
+
+
+/*
+** -------------------------------- DESTRUCTOR --------------------------------
+*/
+
+Convert::~Convert()
+{
+}
+
+
+/*
+** --------------------------------- OVERLOAD ---------------------------------
+*/
+
+Convert &				Convert::operator=( Convert const & rhs )
+{
+	//if ( this != &rhs )
+	//{
+		//this->_value = rhs.getValue();
+	//}
+	return *this;
+}
+
+/* ************************************************************************** */
---
+++ cpp/cpp06/ex00/main.cpp
@@ -0,0 +1,11 @@
+#include "Convert.hpp"
+int	main(int argc, char **argv)
+{
+	Convert a;
+	if (argc != 2)
+		exit(0);
+	
+	
+
+	
+}
